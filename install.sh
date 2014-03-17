0db914fab570ef9f9a9a7d052a6ba9f45ec90df7
Robert-Jan Bijl <rober-jan@prezent.nl>
2014-03-17 14:38:12 +0100

    Added install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.screenrc ~/.screenrc
+ln -s ~/.dotfiles/.profile ~/.profile
+ln -s ~/.dotfiles/.gitignore ~/.gitignore
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
