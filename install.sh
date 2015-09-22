de4cbab198458e1c613a1103e3fe26703f16786b
Robert-Jan Bijl <robert-jan@prezent.nl>
2015-09-22 10:31:39 +0200

    add inputrc file for keyup and down keybindings

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ ln -s ~/.dotfiles/.profile ~/.profile
 ln -s ~/.dotfiles/.gitignore ~/.gitignore
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+
+# keybindings for keyup and keydown
+ln -s ~/.dotfiles/.inputrc ~/.inputrc
+bind -f ~/.inputrc
