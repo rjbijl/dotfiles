f21bc02515fd008ca195c2b6422e043d309b37ca
Robert-Jan Bijl <robert-jan@prezent.nl>
2023-03-15 10:27:30 +0000

    improve install script, create ~/bin

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # download the git prompt helper file
+echo 'Downloading git prompt file'
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
+echo 'Creating symlinks for config files'
 ln -sfn ~/.dotfiles/.bashrc ~/.bashrc
 ln -sfn ~/.dotfiles/.screenrc ~/.screenrc
 ln -sfn ~/.dotfiles/.profile ~/.profile
@@ -9,4 +11,6 @@ ln -sfn ~/.dotfiles/.gitignore ~/.gitignore
 ln -sfn ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sfn ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
+echo 'Adding helper scripts to ~/bin'
+mkdir -p ~/bin
 ln -sfn ~/.dotfiles/gitsync ~/bin/gitsync
